8e091f09a968559d5a6393150d1df8e4dd29d0c5
SantiagoArb <santiagoarb50@gmail.com>
2021-08-21 09:53:03 -0500

    first commit

---
+++ src/main/java/com/devco/seleccionprocesos/SeleccionprocesosApplication.java
@@ -0,0 +1,13 @@
+package com.devco.seleccionprocesos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleccionprocesosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleccionprocesosApplication.class, args);
+	}
+
+}
